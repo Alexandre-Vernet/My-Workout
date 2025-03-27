cfe8743dcca45902817e497ee03032915d029e29
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-03-27 19:08:14 +0100

    feat(primeng): Install PrimeNg

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ListMuscleGroupComponent } from './muscle-group/list-muscle-group/list-muscle-group.component';
-import { ListExercisesComponent } from './exercise/list-exercises/list-exercises.component';
 
 @Component({
-  imports: [RouterModule, ListMuscleGroupComponent, ListExercisesComponent],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
