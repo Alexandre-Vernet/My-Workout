cd95682067d161ebae3c8ce79efa092980272b46
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-04-09 11:59:47 +0200

    feat(exercises): List exercises

--- apps/app/src/app/exercise/exercise.service.ts
+++ apps/app/src/app/exercise/exercise.service.ts
@@ -4,14 +4,13 @@ import { HttpClient } from '@angular/common/http';
 import { Exercise } from '../../../../../libs/interfaces/exercise';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ExerciseService {
-  constructor(private readonly httpClient: HttpClient) {}
+  constructor(private readonly httpClient: HttpClient) {
+  }
 
   listExercise(muscleGroupId: number) {
-    return this.httpClient.get<Exercise[]>(
-      `${environment.API_URL}/exercises?muscleGroup=${muscleGroupId}`
-    );
+    return this.httpClient.get<Exercise[]>(`${ environment.API_URL }/exercises?muscleGroup=${ muscleGroupId }`);
   }
 }
--- apps/app/src/app/exercise/list-exercises/list-exercises.component.ts
+++ apps/app/src/app/exercise/list-exercises/list-exercises.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExerciseService } from '../exercise.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Exercise } from '../../../../../../libs/interfaces/exercise';
 import { switchMap } from 'rxjs';
+import { DataView } from 'primeng/dataview';
 
 @Component({
   selector: 'app-list-exercises',
-  imports: [CommonModule],
+  imports: [CommonModule, DataView, RouterLink],
   templateUrl: './list-exercises.component.html',
   styleUrl: './list-exercises.component.scss'
 })
@@ -25,9 +26,7 @@ export class ListExercisesComponent implements OnInit {
     this.route.params.pipe(
       switchMap((params: { exerciseId: number }) => this.exerciseService.listExercise(params.exerciseId))
     )
-      .subscribe(exercises => {
-        this.exercises = exercises;
-      });
+      .subscribe(exercises => this.exercises = exercises);
   }
 }
 
