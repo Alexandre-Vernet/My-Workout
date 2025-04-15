1eb7cd43494e8ace433d123dbcdacf7e26fd34e4
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-04-15 18:36:53 +0200

    feat(PWA): Install / configure PWA

--- apps/app/src/app/app.component.ts
+++ apps/app/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 import { take } from 'rxjs';
+import { SwPush, SwUpdate } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @Component({
     imports: [RouterModule],
@@ -12,8 +14,33 @@ import { take } from 'rxjs';
 export class AppComponent implements OnInit {
 
     constructor(
-        private readonly authService: AuthService
+        private readonly authService: AuthService,
+        private readonly sw: SwPush,
+        private readonly swUpdate: SwUpdate
     ) {
+        if (environment.production) {
+            // Force refresh PWA
+            this.swUpdate.checkForUpdate();
+            if (this.swUpdate.isEnabled) {
+                this.swUpdate.versionUpdates.subscribe(event => {
+                    if (event.type === 'VERSION_READY') {
+                        window.location.reload();
+                    }
+                });
+            }
+
+            if (!('serviceWorker' in navigator)) {
+                setTimeout(() => {
+                    alert('Service workers are not supported in this browser');
+                }, 1000);
+            }
+
+            if (!this.sw.isEnabled) {
+                setTimeout(() => {
+                    alert('Service workers are not enabled');
+                }, 1000);
+            }
+        }
     }
 
     ngOnInit() {
--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +17,12 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura
       }
-    })
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ],
 };
