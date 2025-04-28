824df3bb9a06a0ce04aa382cedd14a3722977777
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-04-29 01:13:09 +0200

    feat(theme): Add custom preset

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -6,20 +6,41 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 import { provideServiceWorker } from '@angular/service-worker';
+import { definePreset } from '@primeng/themes';
+
+
+const preset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '#e0f7fa',
+            100: '#b2ebf2',
+            200: '#80deea',
+            300: '#4dd0e1',
+            400: '#26c6da',
+            500: '#00bcd4',
+            600: '#00acc1',
+            700: '#0097a7',
+            800: '#00838f',
+            900: '#006064',
+            950: '#004d4f'
+        }
+    }
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
-    provideHttpClient(),
-    provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    }), provideServiceWorker('ngsw-worker.js', {
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(appRoutes),
+        provideHttpClient(),
+        provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: preset
+            }
+        }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
         })
     ]
 };
+
