0cb1215ae8a09b56efddce42435386e544baaa14
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-04-30 00:28:24 +0200

    feat(app): Scroll to top after navigation

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -10,7 +10,9 @@ import { presetCyan } from './theme/preset-cyan';
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(appRoutes),
+        provideRouter(appRoutes, withInMemoryScrolling({
+            scrollPositionRestoration: "top"
+        })),
         provideHttpClient(),
         provideAnimationsAsync(),
         providePrimeNG({
