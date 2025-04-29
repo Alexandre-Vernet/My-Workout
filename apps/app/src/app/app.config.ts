c0f043aa9edd603eaa5d8a1586f30affb71fd805
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-04-29 23:27:04 +0200

    feat(theme): Add custom preset

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -1,31 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
 import { provideServiceWorker } from '@angular/service-worker';
-import { definePreset } from '@primeng/themes';
-
-
-const preset = definePreset(Aura, {
-    semantic: {
-        primary: {
-            50: '#e0f7fa',
-            100: '#b2ebf2',
-            200: '#80deea',
-            300: '#4dd0e1',
-            400: '#26c6da',
-            500: '#00bcd4',
-            600: '#00acc1',
-            700: '#0097a7',
-            800: '#00838f',
-            900: '#006064',
-            950: '#004d4f'
-        }
-    }
-});
+import { presetCyan } from './theme/preset-cyan';
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -35,7 +15,7 @@ export const appConfig: ApplicationConfig = {
         provideAnimationsAsync(),
         providePrimeNG({
             theme: {
-                preset: preset
+                preset: presetCyan
             }
         }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
