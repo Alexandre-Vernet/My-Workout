d478ab459eb5b95b53deaa5bdcd9487526a18713
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-06-20 11:05:15 +0200

    feat(view-exercise): add loading skeleton for image and improve image loading handling

--- apps/app/src/app/navbar/library/view-exercise/view-exercise.component.ts
+++ apps/app/src/app/navbar/library/view-exercise/view-exercise.component.ts
@@ -11,10 +11,11 @@ import { Fieldset } from 'primeng/fieldset';
 import { Tag } from 'primeng/tag';
 import { Drawer } from 'primeng/drawer';
 import { DeviceDetectionService } from '../../../services/device-detection.service';
+import { Skeleton } from 'primeng/skeleton';
 
 @Component({
     selector: 'app-view-exercise',
-    imports: [CommonModule, Button, Fieldset, Tag, Drawer],
+    imports: [CommonModule, Button, Fieldset, Tag, Drawer, Skeleton],
     templateUrl: './view-exercise.component.html',
     styleUrl: './view-exercise.component.scss',
     standalone: true
@@ -29,6 +30,8 @@ export class ViewExerciseComponent implements OnInit {
 
     showImage = true;
     imagePath = '';
+
+    isLoading = true;
 
     constructor(
         private readonly exerciseService: ExerciseService,
