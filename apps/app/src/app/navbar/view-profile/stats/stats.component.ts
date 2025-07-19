2b4a8d885b3c158da3ba9c4684c9bd4314f216d6
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-07-19 12:37:59 +0200

    refactor(graphs, stats): improve layout and loading state in graphs and stats components

--- apps/app/src/app/navbar/view-profile/stats/stats.component.ts
+++ apps/app/src/app/navbar/view-profile/stats/stats.component.ts
@@ -6,10 +6,11 @@ import { RouterLink } from '@angular/router';
 import { WorkoutService } from '../../../services/workout.service';
 import { AlertService } from '../../../services/alert.service';
 import { ThemeService } from '../../../theme/theme.service';
+import { Skeleton } from 'primeng/skeleton';
 
 @Component({
     selector: 'app-stats',
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, Skeleton],
     templateUrl: './stats.component.html',
     styleUrl: './stats.component.scss'
 })
@@ -19,6 +20,7 @@ export class StatsComponent implements OnInit {
 
     totalDaysWorkout = 0;
 
+    isLoading = true;
     isDarkMode = false;
 
     constructor(
@@ -32,7 +34,16 @@ export class StatsComponent implements OnInit {
     ngOnInit() {
         this.exerciseService.findAddedExercises()
             .subscribe({
-                next: (exercises) => this.exercises = exercises
+                next: (exercises) => {
+                    this.isLoading = false;
+                    this.exercises = exercises;
+                },
+                error: (err) => {
+                    this.alertService.alert$.next({
+                        severity: 'error',
+                        message: err?.error?.message ?? 'Erreur lors de la récupération des exercices'
+                    });
+                }
             });
 
         this.countTotalDaysWorkout();
@@ -47,7 +58,7 @@ export class StatsComponent implements OnInit {
                 error: (err) => {
                     this.alertService.alert$.next({
                         severity: 'error',
-                        message: err?.error?.message ?? 'Erreur lors de la récupération des données'
+                        message: err?.error?.message ?? 'Erreur lors de la récupération du nombre de jour d\'entraînement'
                     });
                 }
             });
