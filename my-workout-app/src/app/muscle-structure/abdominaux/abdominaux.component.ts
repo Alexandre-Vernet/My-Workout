2cdff033ba628e5e901bc59b1d6640f85df72392
Alexandre Vernet <alexandre.vernet99@gmail.com>
2025-12-17 19:02:10 +0100

    fix(app): Fix scroll muscle structure

--- my-workout-app/src/app/muscle-structure/abdominaux/abdominaux.component.ts
+++ my-workout-app/src/app/muscle-structure/abdominaux/abdominaux.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
     selector: 'app-abdominaux',
-    imports: [CommonModule, TableModule],
+    imports: [CommonModule, TableModule, IonContent],
     templateUrl: './abdominaux.component.html',
     styleUrl: '../muscle-structure.component.scss',
 })
--- my-workout-app/src/app/muscle-structure/epaules/epaules.component.ts
+++ my-workout-app/src/app/muscle-structure/epaules/epaules.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { IonContent } from "@ionic/angular/standalone";
 
 @Component({
     selector: 'app-epaules',
-    imports: [CommonModule, TableModule],
+    imports: [CommonModule, TableModule, IonContent],
     templateUrl: './epaules.component.html',
     styleUrl: '../muscle-structure.component.scss',
 })
