00e6a3fd00fdc24822334d37020ec947e23664ec
Alexandre Vernet <alexandre.vernet99@gmail.com>
2026-02-28 23:36:41 +0100

    feat(guard): Update message AdminGuard

--- my-workout-api/src/auth/guards/admin.guard.ts
+++ my-workout-api/src/auth/guards/admin.guard.ts
@@ -8,7 +8,7 @@ export class AdminGuard extends AuthGuard('jwt') {
     handleRequest(err: any, user: any, info: any) {
         const castedUser = user as User;
         if (err || info || !castedUser.isAdmin) {
-            throw new ForbiddenException('Vous n\'êtes pas autorisé à ajouter un exercice');
+            throw new ForbiddenException('Vous devez être administrateur pour effectuer cette action');
         }
 
         return user;
