ed1f2625a280625f8fedc0fa3e6dc7e7c22d7505
Alexandre Vernet <alexandre.vernet99@gmail.com>
2026-03-06 19:23:29 +0100

    feat: Init project Spring Boot

---
+++ my-workout-api/src/main/java/com/avernet/my_workout_api/MyWorkoutApiApplication.java
@@ -0,0 +1,13 @@
+package com.avernet.my_workout_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkoutApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkoutApiApplication.class, args);
+	}
+
+}
