eefcb17203888ab27aa8afc3f0cd25235226af43
Alexandre Vernet <alexandre.vernet99@gmail.com>
2026-05-08 23:15:49 +0200

    clean: Allow HEAD request

--- my-workout-api/src/main/java/com/avernet/myworkoutapi/config/SecurityConfig.java
+++ my-workout-api/src/main/java/com/avernet/myworkoutapi/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth ->
                 auth
                     .requestMatchers(HttpMethod.GET, "/").permitAll()
+                    .requestMatchers(HttpMethod.HEAD, "/").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/refresh").permitAll()
