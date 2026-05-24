80bdf81b15a2baaeda9205b8843f7a35b49fc726
Alexandre Vernet <alexandre.vernet99@gmail.com>
2026-05-24 20:48:04 +0200

    feat: Add mode unilateral

--- my-workout-api/src/main/java/com/avernet/myworkoutapi/history/HistoryService.java
+++ my-workout-api/src/main/java/com/avernet/myworkoutapi/history/HistoryService.java
@@ -97,7 +97,9 @@ public class HistoryService {
         HistoryEntity historyEntity = historyRepository.findById(historyId)
             .orElseThrow(() -> new ApiException(ErrorCodeEnum.HISTORY_NOT_FOUND, "Cet historique n'existe pas", HttpStatus.NOT_FOUND));
 
-        if (!history.weight.equals(historyEntity.weight)) {
+        if (history.weight == null || history.weight == 0) {
+            historyEntity.weight = null;
+        } else {
             historyEntity.weight = history.weight;
         }
 
@@ -105,9 +107,6 @@ public class HistoryService {
             historyEntity.reps = history.reps;
         }
 
-        if (history.weight == 0) {
-            historyEntity.weight = null;
-        }
 
         return historyMapper.toDto(historyEntity);
     }
