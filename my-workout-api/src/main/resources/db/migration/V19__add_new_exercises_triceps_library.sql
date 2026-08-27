ec5bac7478c7e297c07156dfff62ce5b7b7e14a2
Alexandre Vernet <alexandre.vernet99@gmail.com>
2026-08-27 12:06:43 +0200

    feat: Add triceps exercises

---
+++ my-workout-api/src/main/resources/db/migration/V19__add_new_exercises_triceps_library.sql
@@ -0,0 +1,257 @@
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (145, 'Dips assistés',
+        '1. Installez-vous sur la machine à dips assistés et placez les genoux ou les pieds sur la plateforme prévue à cet effet, selon le modèle de machine.
+
+2. Saisissez fermement les poignées avec les bras presque tendus et gardez le buste droit, les épaules basses et les coudes légèrement fléchis.
+
+3. Descendez lentement en fléchissant les coudes jusqu''à ce que les bras atteignent une amplitude confortable, sans laisser les épaules remonter excessivement.
+
+4. Poussez sur les poignées en contractant les triceps afin de revenir à la position de départ, sans verrouiller brutalement les coudes.
+
+5. Répétez le mouvement de manière contrôlée en maintenant une trajectoire stable et en laissant la machine fournir l''assistance nécessaire.',
+        'BEGINNER',
+        'COMPOUND');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (145, 2),
+    (145, 27),
+    (145, 28);
+
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (146, 'Développé incliné prise serrée à la barre',
+        '1. Allongez-vous sur un banc incliné, les pieds bien à plat au sol et les omoplates légèrement serrées.
+
+2. Saisissez la barre avec une prise serrée, légèrement plus étroite que la largeur des épaules, puis positionnez-la au-dessus de la partie supérieure de la poitrine, les bras presque tendus.
+
+3. Descendez lentement la barre vers le haut de la poitrine en fléchissant les coudes, tout en gardant ceux-ci proches du corps.
+
+4. Marquez un léger temps d''arrêt en bas du mouvement en conservant le contrôle de la barre et une position stable des épaules.
+
+5. Poussez la barre vers le haut en contractant les triceps et les pectoraux jusqu''à l''extension presque complète des bras, sans verrouiller brutalement les coudes.
+
+6. Répétez le mouvement de manière contrôlée en maintenant les poignets alignés avec les avant-bras et la trajectoire de la barre stable.',
+        'INTERMEDIATE',
+        'COMPOUND');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (146, 2),
+    (146, 27),
+    (146, 28);
+
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (147, 'Extension triceps au-dessus de la tête à la barre',
+        '1. Asseyez-vous sur un banc avec le dos droit et les pieds bien à plat au sol, puis saisissez la barre avec une prise légèrement plus étroite que la largeur des épaules.
+
+2. Amenez la barre au-dessus de votre tête, les bras presque tendus, en gardant les coudes dirigés vers l''avant et proches de la tête.
+
+3. Descendez lentement la barre derrière la tête en fléchissant les coudes, tout en gardant les bras supérieurs relativement immobiles.
+
+4. Marquez un léger temps d''arrêt en position basse en conservant le contrôle de la barre et un étirement des triceps.
+
+5. Tendez les coudes pour ramener la barre au-dessus de la tête en contractant les triceps, sans verrouiller brutalement les coudes.
+
+6. Répétez le mouvement de manière contrôlée en maintenant le buste stable et les coudes dans une position fixe.',
+        'INTERMEDIATE',
+        'ISOLATION');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (147, 2),
+    (147, 27),
+    (147, 28);
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (148, 'Dips au sol',
+        '1. Asseyez-vous au sol devant un banc stable et placez vos mains sur le bord du banc, légèrement plus écartées que la largeur des épaules.
+
+2. Avancez les pieds et soulevez les hanches afin de placer le poids du corps sur les mains, en gardant les jambes tendues ou légèrement fléchies.
+
+3. Descendez lentement le corps vers le sol en fléchissant les coudes, tout en gardant le dos proche du banc et les épaules basses.
+
+4. Poussez sur les mains en contractant les triceps pour remonter le corps jusqu''à l''extension presque complète des bras.
+
+5. Répétez le mouvement de manière contrôlée en gardant les coudes orientés vers l''arrière et le banc stable.',
+        'BEGINNER',
+        'COMPOUND');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (148, 2),
+    (148, 27),
+    (148, 28);
+
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (149, 'Extension triceps à la poulie sur banc incliné',
+        '1. Installez un banc incliné face à une poulie basse et allongez-vous dessus en tenant la poignée de la poulie à deux mains, les bras tendus au-dessus de la tête.
+
+2. Gardez les bras relativement fixes et les coudes orientés vers l''avant, sans les laisser s''écarter excessivement.
+
+3. Fléchissez les coudes pour ramener la poignée derrière ou au-dessus de la tête, en contrôlant la descente et en étirant les triceps.
+
+4. Tendez les coudes pour ramener la poignée à la position de départ en contractant fortement les triceps, sans bouger les épaules.
+
+5. Répétez le mouvement de manière contrôlée en maintenant une tension constante sur les triceps.',
+        'INTERMEDIATE',
+        'ISOLATION');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (149, 2),
+    (149, 27),
+    (149, 28);
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (150, 'Pompes prise serrée',
+        '1. Placez-vous en position de pompe, les mains rapprochées sous la poitrine, légèrement plus serrées que la largeur des épaules.
+
+2. Gardez le corps aligné de la tête aux pieds, les abdominaux et les fessiers contractés.
+
+3. Fléchissez les coudes en les maintenant relativement proches du corps afin de descendre la poitrine vers le sol de manière contrôlée.
+
+4. Poussez sur les mains pour tendre les bras et revenir à la position de départ, en contractant fortement les triceps.
+
+5. Répétez le mouvement sans laisser les hanches s''affaisser ni utiliser d''élan.',
+        'INTERMEDIATE',
+        'COMPOUND');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (150, 2),
+    (150, 27),
+    (150, 28);
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (151, 'Extension triceps avec haltères sur banc incliné',
+        '1. Allongez-vous sur un banc incliné, un haltère dans chaque main, les bras tendus au-dessus de la poitrine et les paumes face à face.
+
+2. Gardez les bras relativement fixes et les coudes orientés vers l''avant pendant toute l''exécution du mouvement.
+
+3. Fléchissez les coudes pour descendre lentement les haltères de chaque côté de la tête, en contrôlant l''étirement des triceps.
+
+4. Tendez les coudes pour ramener les haltères à la position de départ en contractant les triceps, sans bouger excessivement les épaules.
+
+5. Répétez le mouvement de manière contrôlée en maintenant les coudes stables et proches de leur position initiale.',
+        'INTERMEDIATE',
+        'ISOLATION');
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (151, 2),
+    (151, 27),
+    (151, 28);
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (152, 'Développé couché aux haltères prise serrée',
+        '1. Allongez-vous sur un banc avec un haltère dans chaque main, les paumes face à face et les bras tendus au-dessus de la poitrine.
+
+2. Gardez les bras relativement fixes et les coudes proches du corps pendant toute l''exécution du mouvement.
+
+3. Fléchissez les coudes pour abaisser lentement les haltères vers les épaules, sans laisser les bras supérieurs se déplacer excessivement.
+
+4. Marquez une courte pause en bas du mouvement, puis tendez les coudes pour repousser les haltères vers la position de départ en contractant les triceps.
+
+5. Effectuez chaque répétition de manière contrôlée, en gardant les deux haltères alignés et les poignets stables.',
+        'INTERMEDIATE',
+        'ISOLATION');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (152, 2),
+    (152, 27),
+    (152, 28);
+
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (153, 'Extension triceps assis avec haltère',
+        '1. Asseyez-vous sur un banc, le dos droit, en tenant un haltère à deux mains au-dessus de la tête, les bras tendus.
+
+2. Gardez les coudes orientés vers l''avant et proches de la tête pendant toute l''exécution du mouvement.
+
+3. Fléchissez lentement les coudes pour descendre l''haltère derrière la tête, sans bouger excessivement les épaules.
+
+4. Tendez les coudes pour ramener l''haltère à la position de départ en contractant les triceps.
+
+5. Répétez le mouvement de manière contrôlée en gardant le dos stable et les coudes dans une position fixe.',
+        'BEGINNER',
+        'ISOLATION');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (153, 2),
+    (153, 27),
+    (153, 28);
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (154, 'Pompes en équilibre sur les mains',
+        '1. Placez-vous en équilibre sur les mains, idéalement contre un mur pour sécuriser la position, les bras tendus et le corps aligné.
+
+2. Contractez les abdominaux et les fessiers afin de maintenir une position stable et évitez de cambrer excessivement le dos.
+
+3. Fléchissez lentement les coudes pour descendre la tête vers le sol en gardant les coudes relativement proches du corps.
+
+4. Lorsque la tête atteint une profondeur confortable, poussez fortement dans les mains pour tendre les bras et revenir à la position de départ.
+
+5. Gardez le mouvement contrôlé et maintenez l''alignement du corps pendant toute la répétition.',
+        'ADVANCED',
+        'COMPOUND');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (154, 2),
+    (154, 27),
+    (154, 28);
+
+
+INSERT INTO public.exercises
+(id, "name", description, difficulty, mechanic)
+VALUES (155, 'Pompes sur les avant-bras',
+        '1. Placez-vous en position de planche sur les avant-bras, les coudes sous les épaules et les avant-bras posés au sol.
+
+2. Gardez le corps aligné de la tête aux pieds en contractant les abdominaux et les fessiers.
+
+3. Fléchissez les coudes pour abaisser le corps de manière contrôlée, en gardant les coudes proches du corps.
+
+4. Poussez sur les avant-bras pour ramener le corps à la position de départ en contractant les triceps.
+
+5. Répétez le mouvement sans laisser les hanches s''affaisser ni utiliser d''élan.',
+        'BEGINNER',
+        'COMPOUND');
+
+INSERT INTO public.exercise_muscle
+(exercise_id, muscle_id)
+VALUES
+    (155, 2),
+    (155, 27),
+    (155, 28);
+
+UPDATE public.exercises
+SET "name"='Développé couché prise serrée à la barre'
+WHERE id=88;
+
+
+
+SELECT setval('exercises_id_seq', (SELECT MAX(id) FROM exercises));
